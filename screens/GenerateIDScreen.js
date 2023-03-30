7042242b62cc0341431af9d9bb46f1eb389ccdc2
PSC <69509019+Prosted@users.noreply.github.com>
2023-03-31 03:00:25 +0900

    Add Search Address in GenerateIDScreen

--- screens/GenerateIDScreen.js
+++ screens/GenerateIDScreen.js
@@ -1,9 +1,10 @@
 //Import ---------------------------------------------------
 //  React
 import { useState } from "react";
+import Postcode from '@actbase/react-daum-postcode';
 
 //  Native
-import { View, StyleSheet, Pressable, TextInput } from "react-native";
+import { View, StyleSheet, Pressable, TextInput, Modal } from "react-native";
 
 //  Components
 import PrimaryButton from "../components/PrimaryButton";
@@ -22,8 +23,8 @@ function GenerateIDScreen({route, navigation})
     const [birthdate, setBirthdate] = useState(new Date());
     const [currentSelectedGender, setCurrentSelectedGender] = useState('남');
     
-    const [visible, setVisible] = useState(false); // 모달 노출 여부
-    
+    const [visible, setVisible] = useState(false); // 날짜 피커 모달 노출 여부
+    const [isModal, setIsModal] = useState(false); // 주소 모달 노출 여부
     const koreaBirthFormat = dateFormat();
     
     function onPressDate(){ // 날짜 클릭 시
@@ -77,9 +78,26 @@ function GenerateIDScreen({route, navigation})
         setNickname(enteredText);
     }
 
-    function addressHandler(enteredText)
+    const getAddressData = data => {
+        let defaultAddress = ''; // 기본주소
+        if (data.buildingName === 'N') {
+          defaultAddress = data.apartment;
+        } else {
+          defaultAddress = data.buildingName;
+        }
+       
+        //navigation.goBack();
+        setIsModal(false);
+        // route.params.onSelect({
+        //   zone_code: data.zonecode,
+        //   default_address: data.address + ' ' + defaultAddress,
+        // });
+        setAddress(data.address + ' ' + defaultAddress);
+    };
+
+    function AddressModalHandler()
     {
-        setAddress(enteredText);
+        setIsModal(true);
     }
 
     return (
@@ -105,7 +123,24 @@ function GenerateIDScreen({route, navigation})
                         <SmallToggleSwitch handler={SelectGenderHandler} selectedGender={currentSelectedGender}>여</SmallToggleSwitch>
                     </View>
                 </View>
-                <InputField placeholder={"거주지"} maxLength={4} value={address} onChangeText={addressHandler} />
+                <Modal visible={isModal}>
+                    <Postcode
+                        style={{ width: 320, height: 320 }}
+                        jsOptions={{ animation: true, hideMapBtn: true }}
+                        onSelected={getAddressData}
+                    />
+                </Modal>
+                <Pressable onPress={AddressModalHandler} style={styles.datePress}>
+                    <TextInput
+                      pointerEvents="none"
+                      style={styles.textInput}
+                      placeholder={'주소'}
+                      placeholderTextColor="grey"
+                      underlineColorAndroid="transparent"
+                      editable={false}
+                      value={address}
+                    />
+                </Pressable>
             </View>
             <PrimaryButton onPress={MoveToNextScreen}>다음으로</PrimaryButton>
         </View>
