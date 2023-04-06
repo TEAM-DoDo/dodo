86f091ccb3da512ec34134510508598db0aa858c
Aiden_MIN <tlatnals123@gmail.com>
2023-04-06 17:34:30 +0900

    add vector-icons library

--- screens/SelectCategoryScreen.js
+++ screens/SelectCategoryScreen.js
@@ -1,17 +1,24 @@
 //Import ---------------------------------------------------
 //  React
 import { useState } from "react";
+import React, { Component } from "react";
 
 //  Native
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, Button, StyleSheet, Pressable } from "react-native";
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 //Definition Component ---------------------------------------------------
-function SelectCategoryScreen({route, navigation})
-{
+function SelectCategoryScreen({ route, navigation }) {
     const userInfo = route.params.userInfo;
     console.log(userInfo);
 
-    return(
+    return (
         <View>
             <Text>This is SelectCategoryScreen!</Text>
         </View>
