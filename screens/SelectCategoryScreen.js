f481ee866ca4a6ba946049f728ff01d87a7898d8
Aiden_MIN <tlatnals123@gmail.com>
2023-04-06 19:57:25 +0900

    SelectCategoryScreen.js 일부 함수 추가, test 결과: return 값이 없어서 오류x

--- screens/SelectCategoryScreen.js
+++ screens/SelectCategoryScreen.js
@@ -1,22 +1,101 @@
 //Import ---------------------------------------------------
 //  React
 import { useState } from "react";
-import React, { Component } from "react";
+
 
 //  Native
-import { View, Text, Button, StyleSheet, Pressable } from "react-native";
-import { Fontisto } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
+import { TouchableOpacity, View, Text, Button, StyleSheet, Pressable } from "react-native";
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 
 //Definition Component ---------------------------------------------------
 function SelectCategoryScreen({ route, navigation }) {
     const userInfo = route.params.userInfo;
     console.log("SelectCategoryScreen에서 출력 : ", userInfo);
+    const [selectedIcons, setSelectedIcons] = useState([]);
+
+    const icons = [
+        {
+            name: "Fontisto",
+            data: [
+                { iconName: "paper-plane", label: "Paper plane" },
+                { iconName: "baidu", label: "Baidu" },
+                { iconName: "bell-alt", label: "Bell Alt" },
+            ],
+        },
+        {
+            name: "MaterialIcons",
+            data: [
+                { iconName: "brush", label: "Brush" },
+                { iconName: "book", label: "Book" },
+                { iconName: "backup", label: "Backup" },
+            ],
+        },
+        {
+            name: "FontAwesome5",
+            data: [
+                { iconName: "language", label: "Language" },
+                { iconName: "feather-alt", label: "Feather alt" },
+                { iconName: "apple-alt", label: "Apple Alt" },
+            ],
+        },
+        {
+            name: "MaterialCommunityIcons",
+            data: [
+                { iconName: "dance-ballroom", label: "Dance ballroom" },
+                { iconName: "dumbbell", label: "Dumbbell" },
+                { iconName: "bomb", label: "Bomb" },
+            ],
+        },
+        {
+            name: "Ionicons",
+            data: [
+                { iconName: "earth", label: "Earth" },
+                { iconName: "fast-food", label: "Fast food" },
+                { iconName: "ios-game-controller", label: "Ios game controller" },
+            ],
+        },
+        {
+            name: "FontAwesome",
+            data: [
+                { iconName: "handshake-o", label: "Handshake" },
+                { iconName: "fast-food", label: "Fast food" },
+                { iconName: "cafe", label: "Cafe" },
+            ],
+        },
+        {
+            name: "Entypo",
+            data: [
+                { iconName: "dots-three-horizontal", label: "Dots three horizontal" },
+                { iconName: "archive", label: "Archive" },
+                { iconName: "back-in-time", label: "Back In Time" },
+            ],
+        },
+    ];
+
+    function toggleIconSelection(iconName) {
+        setSelectedIcons((prevState) => {
+            if (prevState.includes(iconName)) {
+                return prevState.filter((name) => name !== iconName);
+            } else {
+                return [...prevState, iconName];
+            }
+        });
+    }
+
+    function moveToSelectTrendCategoryScreen() {
+        if (selectedIcons.length >= 2) {
+          navigation.navigate("SelectTrendCategoryScreen", { selectedIcons });
+        } else {
+          alert("Please select at least two icons.");
+        }
+      }
 
     return (
         <View>
