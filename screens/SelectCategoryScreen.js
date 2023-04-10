3a19e30a0a653ae9aac52b81973a54000b06c3fe
Aiden_MIN <tlatnals123@gmail.com>
2023-04-10 18:49:11 +0900

    delete directory, vector-icons 시험출력

--- screens/SelectCategoryScreen.js
+++ screens/SelectCategoryScreen.js
@@ -5,13 +5,7 @@ import { useState } from "react";
 
 //  Native
 import { TouchableOpacity, View, Text, Button, StyleSheet, Pressable } from "react-native";
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Entypo from 'react-native-vector-icons/Entypo';
+import { Ionicons } from '@expo/vector-icons';
 
 
 //Definition Component ---------------------------------------------------
@@ -20,64 +14,6 @@ function SelectCategoryScreen({ route, navigation }) {
     console.log("SelectCategoryScreen에서 출력 : ", userInfo);
     const [selectedIcons, setSelectedIcons] = useState([]);
 
-    const icons = [
-        {
-            name: "Fontisto",
-            data: [
-                { iconName: "paper-plane", label: "Paper plane" },
-                { iconName: "baidu", label: "Baidu" },
-                { iconName: "bell-alt", label: "Bell Alt" },
-            ],
-        },
-        {
-            name: "MaterialIcons",
-            data: [
-                { iconName: "brush", label: "Brush" },
-                { iconName: "book", label: "Book" },
-                { iconName: "backup", label: "Backup" },
-            ],
-        },
-        {
-            name: "FontAwesome5",
-            data: [
-                { iconName: "language", label: "Language" },
-                { iconName: "feather-alt", label: "Feather alt" },
-                { iconName: "apple-alt", label: "Apple Alt" },
-            ],
-        },
-        {
-            name: "MaterialCommunityIcons",
-            data: [
-                { iconName: "dance-ballroom", label: "Dance ballroom" },
-                { iconName: "dumbbell", label: "Dumbbell" },
-                { iconName: "bomb", label: "Bomb" },
-            ],
-        },
-        {
-            name: "Ionicons",
-            data: [
-                { iconName: "earth", label: "Earth" },
-                { iconName: "fast-food", label: "Fast food" },
-                { iconName: "ios-game-controller", label: "Ios game controller" },
-            ],
-        },
-        {
-            name: "FontAwesome",
-            data: [
-                { iconName: "handshake-o", label: "Handshake" },
-                { iconName: "fast-food", label: "Fast food" },
-                { iconName: "cafe", label: "Cafe" },
-            ],
-        },
-        {
-            name: "Entypo",
-            data: [
-                { iconName: "dots-three-horizontal", label: "Dots three horizontal" },
-                { iconName: "archive", label: "Archive" },
-                { iconName: "back-in-time", label: "Back In Time" },
-            ],
-        },
-    ];
 
     function toggleIconSelection(iconName) {
         setSelectedIcons((prevState) => {
@@ -96,17 +32,18 @@ function SelectCategoryScreen({ route, navigation }) {
             alert("Please select at least two icons.");
         }
     }
+    
+    function pressHandler(){
+        console.log('Pressed!');
+    }
 
     return (
-        <View>
-            <Text>{userInfo.gender}</Text>
-            <Text>{userInfo.address}</Text>
-            <Text>{userInfo.birthdate}</Text>
+        <View style = {styles.container}>
+            <Text>키워드</Text>
+            <Pressable style={styles.button} onPress={pressHandler}>
+                <Ionicons name="airplane" size={24} color="white"/>
+            </Pressable>
         </View>
-        
-        
-       
-
     );
 }
 
@@ -115,5 +52,12 @@ export default SelectCategoryScreen;
 //style ---------------------------------------------------
 
 const styles = StyleSheet.create({
-    
+    container:{
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    }
+
 });
