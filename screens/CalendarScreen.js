ea1e7e4364145b6a74a47c798e911c5c733e2f28
huiwoo <97345007+huiwoong@users.noreply.github.com>
2023-04-13 07:17:45 +0900

    CalendarScreen
    CalendarScreen.js add Modal component

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,11 +1,12 @@
 //npm install --save react-native-calendars
 //yarn add react-native-calendars
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Modal, Button } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
-const CalendarScreen = () => {
+const App = () => {
   const [selectedDate, setSelectedDate] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleDayPress = (day) => {
     setSelectedDate(day.dateString);
@@ -18,7 +19,16 @@ const CalendarScreen = () => {
         onDayPress={handleDayPress}
         markedDates={{ [selectedDate]: { selected: true } }}
       />
-      <Text style={styles.selectedDate}>{selectedDate}</Text>
+      <Text style={styles.selectedDate} onPress={() => setModalVisible(true)}>
+        {selectedDate}
+      </Text>
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Selected Date</Text>
+          <Text style={styles.modalText}>{selectedDate}</Text>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -26,8 +36,8 @@ const CalendarScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#ffffff',
   },
   title: {
@@ -39,6 +49,20 @@ const styles = StyleSheet.create({
     marginTop: 20,
     fontSize: 18,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
 });
 
-export default CalendarScreen;
+export default App;
