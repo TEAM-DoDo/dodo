499b7f172f3b3d9a48d5d5c1f621b74ae93ca0ff
PSC <69509019+Prosted@users.noreply.github.com>
2023-05-04 21:02:43 +0900

    modify API module

--- api/API.js
+++ api/API.js
@@ -1,18 +1,25 @@
 //Import ---------------------------------------------------
+//  Axios
 import axios from "axios";
+
+//  Native
 import { Platform } from "react-native";
+
+//  Expo
 import Constants from 'expo-constants';
 
+//URL setting ------------------------------------------------------
 const { manifest } = Constants;
-const localIpAddress = manifest.debuggerHost.split(":").shift();
+const localIpAddress = manifest.debuggerHost.split(":").shift(); //현재 ip주소
 
 const portNumber = 8080;
 const platformHTTP = Platform.select({
     android : "http",
-    ios : "https",
+    ios : "http",
 });
 const baseURL = `${platformHTTP}://${localIpAddress}:${portNumber}`;
 
+//Cretae axios module ------------------------------------------------------
 const API = axios.create({
     baseURL,
     headers : {
