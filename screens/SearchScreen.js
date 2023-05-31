847013d9f11640fd8348a1e5980351717b445b61
postive12 <postive12@gmail.com>
2023-05-31 19:32:43 +0900

    Update type search

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -3,29 +3,21 @@ import TopBar from "../components/hgp/TopBar";
 import DoButton from "../components/hgp/DoButton";
 import { useState } from "react";
 import API from "../api/API";
-import { MultipleSelectList } from "react-native-dropdown-select-list";
+import ListSelectModal from "../components/hgp/ListSelectModal";
 
 function SearchScreen({ navigation }){
+    const [searchTypeModalVisible, setSearchTypeModalVisible] = useState(false);
+    const [searchType, setSearchType] = useState("이름"); // ["이름","설명","지역"
     const [doList, setDoList] = useState([]);
-    const [searchName, setSearchNameText] = useState("");
-    const [searchDescription, setSearchDescriptionText] = useState("");
-    const [searchPlace, setSearchPlaceText] = useState("");
+    const [searchText, setSearchText] = useState("");
     const [searchCategory, setSearchCategoryText] = useState("");
-    const [selected, setSelected] = useState([]);
-    console.log(selected);
-    const data = [
-        {key:'1', value:'이름'},
-        {key:'2', value:'설명'},
-        {key:'3', value:'지역'},
-    ]
     const onGoBackPressed = () => {
         dismissKeyboard();
         setTimeout(() => {navigation.goBack();}, 50);
     }
     const onPressSearch = () => {
         dismissKeyboard();
-        console.log("searchText : " + searchName);
-        API.get(`/api/search/do?name=${searchName}&description=${searchDescription}&place=${searchPlace}&category=${searchCategory}`).then((res) => {
+        API.get(`/api/search/do?name=${searchType=="이름"?searchText:""}&description=${searchType=="설명"?searchText:""}&place=${searchType=="지역"?searchText:""}&category=${searchCategory}`).then((res) => {
             console.log(res.data);
             setDoList(res.data);
         }).catch((err) => {
@@ -35,23 +27,30 @@ function SearchScreen({ navigation }){
     const dismissKeyboard = () => {
         Keyboard.dismiss();
     };
+    const onPressSearchType = () => {
+        setSearchTypeModalVisible(true);
+    }
+    const onSelectSearchType = (type) => {
+        setSearchType(type);
+        setSearchTypeModalVisible(false);
+    }
+    const onCancelSearchType = () => {
+        setSearchTypeModalVisible(false);
+    }
     return (
         <View style={Style.container}>
             <TopBar title="검색" onGoBackPressed={onGoBackPressed} enableAlarmButton={false}/>
             <View style={Style.search_holder}>
                 <View style={Style.search_box_holder}>
-                    <TextInput style={Style.search_textfield} placeholder="검색어를 입력하세요." value={searchName} onChangeText={setSearchNameText}/>
+                    <Pressable style={Style.search_type_button} onPress={onPressSearchType}>
+                        <Text style={Style.search_type_style}>{searchType}</Text>
+                    </Pressable>
+                    <TextInput style={Style.search_textfield} placeholder="검색어를 입력하세요." value={searchText} onChangeText={setSearchText}/>
                 </View>
                 <Pressable style={Style.search_button} onPress={onPressSearch}>
                     <Text style={Style.search_button_text}>검색</Text>
                 </Pressable>
             </View>
-            {/* <MultipleSelectList
-                    setSelected={(val) => setSelected(val)}
-                    data={data}
-                    save="value"
-                    onSelect={() => {}}
-                    label="Categories"/> */}
             <FlatList
                 style={Style.list_holder}
                 data={doList}
@@ -59,6 +58,12 @@ function SearchScreen({ navigation }){
                 numColumns={1}
                 renderItem={({item}) => <DoButton navigation={navigation} doId={item}/>}
             />
+            <ListSelectModal
+                data={["이름","설명","지역"]}
+                onSelect={onSelectSearchType}
+                onCancel={onCancelSearchType} 
+                isVisible={searchTypeModalVisible}
+                />
         </View>
     );
 }
@@ -87,6 +92,21 @@ const Style = StyleSheet.create({
         flexDirection:'row',
         alignItems:'center',
         //backgroundColor: 'grey',
+    },
+    search_type_style : {
+        fontSize : 12,
+        fontFamily : 'NanumGothic-ExtraBold',
+        color:"gray",
+    },
+    search_type_button : {
+        width:50,
+        alignSelf:'stretch',
+        borderRadius : 20,
+        borderColor : '#c5c5c5',
+        borderWidth : 1,
+        margin:5,
+        justifyContent:'center',
+        alignItems:'center',
     },
     search_textfield : {
         flex:1,
