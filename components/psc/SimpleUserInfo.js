46c24dad91272f0eba0bc0be67b9fa6ce2f436bc
PSC <69509019+Prosted@users.noreply.github.com>
2023-06-01 19:40:57 +0900

    add circle user image

--- components/psc/SimpleUserInfo.js
+++ components/psc/SimpleUserInfo.js
@@ -4,7 +4,7 @@ import CircleUserImage from '../hgp/CircleUserImage';
 const SimpleUserInfo = ({userInfo}) => {
     return(
         <View style={styles.contentContainer}>
-            <CircleUserImage mode='tiny' margin={5} index={userInfo.id}/>
+            <CircleUserImage mode='tiny' margin={5} index={userInfo.id} imagePath={userInfo.imagePath} />
             <Text style={styles.content}>{userInfo.nickname}</Text>
         </View>
     );
